f34e367188395fc425b1c048bf09d49b0ad1d556
ronilkansoda <ronilkansoda73@gmail.com>
2024-11-08 16:01:53 +0530

    removing "" in main page

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <p className="text-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] text-5xl">
-        Ronil : " My First Project "
+        This is Ronil&apos;s &quot;awesome&quot; profile
       </p>
     </>
   );
