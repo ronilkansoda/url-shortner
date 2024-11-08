c43120144529b7fe5c3cd21091bee2ef58992f56
ronilkansoda <ronilkansoda73@gmail.com>
2024-11-08 17:10:15 +0530

    updating getDomain

--- src/app/lib/getDomain.js
+++ src/app/lib/getDomain.js
@@ -1,5 +1,15 @@
+// export default function getDomain() {
+//     const protocol = process.env.NEXT_PUBLIC_VERCEL_ENV == "production" ? "http" : "https";
+//     const domain = process.env.NEXT_PUBLIC_VERCEL_URL ? process.env.NEXT_PUBLIC_VERCEL_URL : "localhost:3000"
+//     return `${protocol}://${domain}`
+// }
+
 export default function getDomain() {
-    const protocol = process.env.NEXT_PUBLIC_VERCEL_ENV == "production" ? "http" : "https";
-    const domain = process.env.NEXT_PUBLIC_VERCEL_URL ? process.env.NEXT_PUBLIC_VERCEL_URL : "localhost:3000"
-    return `${protocol}://${domain}`
+    const isProduction = process.env.NEXT_PUBLIC_VERCEL_ENV === "production" || process.env.NEXT_PUBLIC_VERCEL_ENV === "preview";
+
+    if (isProduction && process.env.NEXT_PUBLIC_VERCEL_URL) {
+        return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    }
+
+    return "http://localhost:3000";
 }
