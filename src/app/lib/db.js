8b80d3f11eba5d9cffef83d5885f6fa985f96993
ronilkansoda <ronilkansoda73@gmail.com>
2024-11-15 19:15:58 +0530

    making schema file and url varibale in drizzle.config.json

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -8,3 +8,20 @@ export async function helloworld() {
     const dbNow = dbResponse && dbResponse.now ? dbResponse.now : ""; // this will gives me string
     return dbNow
 }
+
+async function configureDatabase() {
+    try {
+        const [dbResponse] = await sql`CREATE TABLE IF NOT EXISTS "links" (
+            "id" serial PRIMARY KEY NOT NULL,
+            "url" text NOT NULL,
+            "short" varchar(50),
+            "created_at" timestamp DEFAULT now()
+        );`
+        console.log("Db response for new table: ", dbResponse);
+    } catch (err) {
+        console.error("Database error during table creation: ", err);
+    }
+}
+
+
+configureDatabase().catch(err => console.log("db config err ", err))
