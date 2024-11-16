ea889f59c9ab82ab7327db070f6c842c8613b6c6
ronilkansoda <ronilkansoda73@gmail.com>
2024-11-16 23:06:51 +0530

    solving error for deployment7

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,4 @@
-import webpack from 'webpack';
-import process from 'process/browser.js'; // Ensure process is correctly imported
-
-const nextConfig = {
-    webpack: (config) => {
-        config.resolve = {
-            ...config.resolve,
-            fallback: {
-                ...config.resolve.fallback,
-                crypto: require.resolve('crypto-browserify'),
-                stream: require.resolve('stream-browserify'),
-                buffer: require.resolve('buffer'),
-                process: require.resolve('process/browser.js'),
-            },
-        };
-
-        config.plugins.push(
-            new webpack.ProvidePlugin({
-                Buffer: ['buffer', 'Buffer'],
-                process: 'process/browser.js', // Ensure process is provided correctly
-            })
-        );
-
-        return config;
-    },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
